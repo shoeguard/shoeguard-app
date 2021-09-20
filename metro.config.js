07351012efeaf98a0758b576f290fe47144c8b13
gtg7784 <tae.gun7784@gmail.com>
2021-09-20 19:39:35 +0900

    chore(metro): update transformer, resolver

--- metro.config.js
+++ metro.config.js
@@ -1,17 +1,32 @@
-/**
- * Metro configuration for React Native
- * https://github.com/facebook/react-native
- *
- * @format
- */
+const path = require('path');
+const {getDefaultConfig} = require('metro-config');
 
-module.exports = {
-  transformer: {
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: true,
+module.exports = (async () => {
+  const {
+    resolver: {sourceExts, assetExts},
+  } = await getDefaultConfig();
+  return {
+    transformer: {
+      getTransformOptions: async () => ({
+        transform: {
+          experimentalImportSupport: false,
+          inlineRequires: false,
+        },
+      }),
+      babelTransformerPath: require.resolve('react-native-svg-transformer'),
+    },
+    resolver: {
+      extraNodeModules: {
+        utils: path.resolve(__dirname, 'utils'),
+        screens: path.resolve(__dirname, 'screens'),
+        components: path.resolve(__dirname, 'components'),
+        api: path.resolve(__dirname, 'api'),
+        hooks: path.resolve(__dirname, 'hooks'),
+        styles: path.resolve(__dirname, 'styles'),
+        images: path.resolve(__dirname, 'assets/images'),
       },
-    }),
-  },
-};
+      assetExts: assetExts.filter(ext => ext !== 'svg'),
+      sourceExts: [...sourceExts, 'svg'],
+    },
+  };
+})();
