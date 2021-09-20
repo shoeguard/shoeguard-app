4baaaa2639160608d4cb6038ca2b5c1d7ed6ff80
gtg7784 <tae.gun7784@gmail.com>
2021-09-20 19:47:02 +0900

    chore(app): add themprovider, statusbar color, navigation

--- App.tsx
+++ App.tsx
@@ -6,17 +6,45 @@
  */
 
 import React, {useEffect} from 'react';
-import {SafeAreaView, StatusBar} from 'react-native';
+import {StatusBar} from 'react-native';
+import {ThemeProvider} from 'styled-components';
 import SplashScreen from 'react-native-splash-screen';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {Onboarding} from 'screens/LoginStack';
+import theme from 'styles/theme';
+
+const RootStack = createNativeStackNavigator();
+const LoginStack = createNativeStackNavigator();
+
+const LoginNavigator = () => (
+  <LoginStack.Navigator
+    initialRouteName="Onboading"
+    screenOptions={{
+      headerShown: false,
+    }}>
+    <LoginStack.Screen name="Onboading" component={Onboarding} />
+  </LoginStack.Navigator>
+);
 
 const App = () => {
   useEffect(() => {
     SplashScreen.hide();
-  });
+  }, []);
+
   return (
-    <SafeAreaView>
+    <ThemeProvider theme={theme}>
       <StatusBar barStyle={'dark-content'} />
-    </SafeAreaView>
+      <NavigationContainer>
+        <RootStack.Navigator
+          initialRouteName="LoginStack"
+          screenOptions={{
+            headerShown: false,
+          }}>
+          <RootStack.Screen name="LoginStack" component={LoginNavigator} />
+        </RootStack.Navigator>
+      </NavigationContainer>
+    </ThemeProvider>
   );
 };
 
