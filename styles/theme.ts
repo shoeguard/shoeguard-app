b41986075003fc9634f48ee6e4dd6b361f9c635a
gtg7784 <tae.gun7784@gmail.com>
2021-09-20 19:41:09 +0900

    chore(style): add theme

---
+++ styles/theme.ts
@@ -0,0 +1,34 @@
+const theme = {
+  color: {
+    blue: '#2e6eed',
+    black: '#000000',
+    white: '#ffffff',
+    gray: '#b8b8b8',
+    red: '#f45151',
+    green: '#22d454',
+  },
+  fontSize: {
+    xl: 24,
+    lg: 20,
+    md: 16,
+    sm: 14,
+    xs: 12,
+    xxs: 8,
+  },
+  fontFamily: {
+    black: 'NotoSansCJKkr-Black',
+    bold: 'NotoSansCJKkr-Bold',
+    demiLight: 'NotoSansCJKkr-DemiLight',
+    light: 'NotoSansCJKkr-Light',
+    medium: 'NotoSansCJKkr-Medium',
+    regular: 'NotoSansCJKkr-Regular',
+    thin: 'NotoSansCJKkr-Thin',
+    monoBold: 'NotoSansMonoCJKkr-Bold',
+    monoRegular: 'NotoSansMonoCJKkr-Regular',
+  },
+};
+
+type ThemeType = typeof theme;
+
+export type {ThemeType};
+export default theme;
