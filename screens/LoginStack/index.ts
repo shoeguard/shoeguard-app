28a3bf9aadaf83b6ea916702ecacb7965728fa56
gtg7784 <tae.gun7784@gmail.com>
2021-09-22 03:03:14 +0900

    chore(screen): update login stack index

--- screens/LoginStack/index.ts
+++ screens/LoginStack/index.ts
@@ -1,3 +1,17 @@
+import NameInput from './NameInput';
 import Onboarding from './Onboarding';
+import PasswordInput from './PasswordInput';
+import PhoneInput from './PhoneInput';
+import SignUpSuccess from './SignUpSuccess';
+import SmsAuthenticate from './SmsAuthenticate';
+import Terms from './Terms';
 
-export {Onboarding};
+export {
+  Onboarding,
+  Terms,
+  PhoneInput,
+  SmsAuthenticate,
+  PasswordInput,
+  NameInput,
+  SignUpSuccess,
+};
