5a3a89469aeb3e497df421dbe72f295debeca026
gtg7784 <tae.gun7784@gmail.com>
2021-09-22 02:58:46 +0900

    chore(component): add isInputFocused props to button

--- components/Button.tsx
+++ components/Button.tsx
@@ -7,29 +7,38 @@ interface IProps {
   label?: string;
   disabled?: boolean;
   color?: string;
+  isInputFocused?: boolean;
 }
 
 interface IStyledButtonProps {
   color?: string;
+  isInputFocused: boolean;
 }
 
 const Button = ({
   onPress = () => {},
   label = '',
   disabled = false,
+  isInputFocused = false,
   color,
+  ...props
 }: IProps) => {
   return (
-    <StyledButton onPress={onPress} disabled={disabled} color={color}>
+    <StyledButton
+      onPress={onPress}
+      disabled={disabled}
+      color={color}
+      isInputFocused={isInputFocused}
+      {...props}>
       <StyledLabel>{label}</StyledLabel>
     </StyledButton>
   );
 };
 
 const StyledButton = styled.TouchableOpacity<IStyledButtonProps>`
-  width: ${wp('91.4%')}px;
+  width: ${({isInputFocused}) => (isInputFocused ? wp('100%') : wp('91.4%'))}px;
   height: ${hp('5.9%')}px;
-  border-radius: 8px;
+  border-radius: ${({isInputFocused}) => (isInputFocused ? '0px' : '8px')};
   background-color: ${({theme, color}) => color || theme.color.gray};
   display: flex;
   align-items: center;
