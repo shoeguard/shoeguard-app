83d5a3f82d20ad490d6b36182b0ad9d2a22ce83e
gtg7784 <tae.gun7784@gmail.com>
2021-09-22 03:00:39 +0900

    chore(component): add text input

---
+++ components/TextInput.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import {TextInputProps} from 'react-native';
+import {useTheme} from 'styled-components';
+import styled from 'styled-components/native';
+import {hp, wp} from 'styles/size';
+
+const TextInput = (props: TextInputProps) => {
+  const theme = useTheme();
+
+  return <Container placeholderTextColor={theme.color.gray} {...props} />;
+};
+
+const Container = styled.TextInput`
+  width: ${wp('91.4%')}px;
+  height: ${hp('5.9%')}px;
+  border-radius: 4px;
+  border: 1px solid ${({theme}) => theme.color.gray};
+  padding: 0 ${wp('4.3%')}px;
+  color: ${({theme}) => theme.color.black};
+  font-size: ${({theme}) => theme.fontSize.sm}px;
+  font-family: ${({theme}) => theme.fontFamily.regular};
+`;
+
+export default TextInput;
