0c674b70350d3867523c9f9ad76a85501606c620
gtg7784 <tae.gun7784@gmail.com>
2021-09-24 15:40:53 +0900

    chore(android): code push setup

--- android/app/src/main/java/com/shoeguard/MainApplication.java
+++ android/app/src/main/java/com/shoeguard/MainApplication.java
@@ -8,6 +8,7 @@ import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.microsoft.codepush.react.CodePush;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
@@ -27,6 +28,11 @@ public class MainApplication extends Application implements ReactApplication {
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
           return packages;
+        }
+
+        @Override
+        protected String getJSBundleFile() {
+            return CodePush.getJSBundleFile();
         }
 
         @Override
